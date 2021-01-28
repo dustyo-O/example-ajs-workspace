6ae4bd63af2959d58a653205a4a746b39d620a09
dusty <dusty@yandex-team.ru>
2021-01-28 19:40:55 +0300

    initial commit

--- index.js
+++
@@ -1,3 +0,0 @@
-const ololo = require('./ololo.js');
-
-ololo();
--- ololo.js
+++
@@ -1,9 +0,0 @@
-function getText() {
-  return 'ololo';
-}
-
-function ololo() {
-  console.log(getText());
-}
-
-module.exports = ololo;
